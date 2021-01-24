1526ada418aa13d1630eee6e43e2d28892c64f22
johnsjoo <70665563+johnsjoo@users.noreply.github.com>
2021-01-24 17:47:17 +0100

    Initial commit

---
+++ InsertDataWeatherApp/Models/Entities/EfContext.cs
@@ -0,0 +1,28 @@
+﻿using InsertDataWeatherApp.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InsertDataWeatherApp.Entities
+{
+    class EfContext : DbContext
+    {
+        string connectionString = string.Empty;
+
+        public EfContext() : base()
+        {
+            ConfigurationBuilder builder = new ConfigurationBuilder();
+            builder.AddJsonFile("appsettings.json", optional: false);
+            var configuration = builder.Build();
+            connectionString = configuration.GetConnectionString("MyConnectionString");
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+        
+        public DbSet<WeatherData> WeatherDataInfo { get; set; }
+    }
+}
---
+++ InsertDataWeatherApp/Models/Entities/WeatherData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InsertDataWeatherApp.Models.Entities
+{
+    public class WeatherData
+    {
+        public int Id { get; set; }
+        public DateTime DateAndTime { get; set; }
+        public string Location { get; set; }
+        public decimal Temp { get; set; }
+        public int Humidity { get; set; }
+
+
+    }
+}
