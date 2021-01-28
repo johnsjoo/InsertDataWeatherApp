09dcea3d1a5b81652f4ea66e98b33baf7bb6a0ef
johnsjoo <70665563+johnsjoo@users.noreply.github.com>
2021-01-28 13:36:14 +0100

    Final version of WeatherApp

--- InsertDataWeatherApp/MainMenuMethods.cs
+++ InsertDataWeatherApp/MainMenuMethods.cs
@@ -16,7 +16,7 @@ namespace InsertDataWeatherApp
         static string filePath = @"D:\It-Högskolan\Blazor\Inlämningsuppgift\InsertDataWeatherApp\InsertDataWeatherApp\File\TemperaturData.csv";
         static bool exit = true;
 
-        //ReadCSVFile(filePath);s
+        //ReadCSVFile(filePath);
         public static void Run()
         {
             while (exit)
@@ -134,6 +134,7 @@ namespace InsertDataWeatherApp
                                 break;
                             }
                         }
+                         
                         ContinueMethod();
                         break;
 
@@ -211,12 +212,127 @@ namespace InsertDataWeatherApp
 
             }
         }
+        public static List<string> FromWarmToCold(string location)
+        {
+            using (var context = new EfContext())
+            {
+                List<string> avgTemperatures = new List<string>();
+
+                //väljer ut de poster beroende av vilken sträng input vi får (inne eller ute)
+                //Sen grupperar vi det på datum-fältet. 
+                var resultSet = context.WeatherDataInfo
+                    .Where(x => x.Location == location)
+                    .GroupBy(x => x.DateAndTime.Date)
+                    .Select(x => new { DateAndTime = x.Key, AvgTemp = x.Average(x => x.Temp) }) //Hämtar ut medeltemp för de grupperande datumen.
+                    .OrderByDescending(x => x.AvgTemp);
+
+                var topResult = resultSet
+                    .Take(10);
+                var bottomResult = resultSet
+                    .OrderBy(x => x.AvgTemp)
+                    .Take(10)
+                    .OrderByDescending(x => x.AvgTemp);
+
+                InsideorOutSide(location);
+                avgTemperatures.Add("Tio dagarna med högst temp");
+                avgTemperatures.Add("**************************");
+                foreach (var result in topResult)
+                {
+                    avgTemperatures.Add($"{result.DateAndTime:yyyy-MM-dd} \t {Math.Round(result.AvgTemp, 1)} °C");//skriver in värdena till strängar i listan.
+                }
+                avgTemperatures.Add("\n");
+                avgTemperatures.Add("Tio dagarna med lägst temp");
+                avgTemperatures.Add("**************************");
+                foreach (var result in bottomResult)
+                {
+                    avgTemperatures.Add($"{result.DateAndTime:yyyy-MM-dd} \t {Math.Round(result.AvgTemp, 1)} °C");
+                }
+                return avgTemperatures;
+            }
+        }
+        public static List<string> HumidMethod(string location)
+        {
+            using (var context = new EfContext())
+            {
+                List<string> avgHumidity = new List<string>();
+
+                var resultSet = context.WeatherDataInfo
+                    .Where(x => x.Location == location)
+                    .GroupBy(x => x.DateAndTime.Date)
+                    .Select(x => new { DateAndTime = x.Key, AVGHumid = x.Average(x => x.Humidity) });
+
+
+                var topResult = resultSet
+                    .OrderByDescending(x => x.AVGHumid)
+                    .Take(15);
+                var bottomResult = resultSet
+                    .OrderBy(x => x.AVGHumid)//För att få de nedersta raderna.
+                    .Take(15)
+                    .OrderByDescending(x => x.AVGHumid);//För att få de nedersta raderna i fallande ordning.
+
+                InsideorOutSide(location);
+                avgHumidity.Add("Tio dagarna med högst fuktighet");
+                avgHumidity.Add("*******************************");
+                foreach (var result in topResult)
+                {
+                    avgHumidity.Add($"{result.DateAndTime:yyyy-MM-dd} \t {Math.Round(result.AVGHumid, 1)} %");
+                }
+                avgHumidity.Add("\n");
+                avgHumidity.Add("Tio dagarna med lägst fuktighet");
+                avgHumidity.Add("*******************************");
+
+                foreach (var result in bottomResult)
+                {
+                    avgHumidity.Add($"{result.DateAndTime:yyyy-MM-dd} \t {Math.Round(result.AVGHumid, 1)} %");
+                }
+                return avgHumidity;
+            }
+
+        }
+        private static bool MetrologicalSeasonWinter()
+        {
+            using (var context = new EfContext())
+            {
+                var q1 = context.WeatherDataInfo
+                    .Where(x => x.Location == "Ute" && x.DateAndTime > DateTime.Parse("2016-08-01"))
+                    .GroupBy(x => x.DateAndTime.Date)
+                    .Select(x => new { day = x.Key, TempAvg = x.Average(x => x.Temp) });
+
+                int turnCounter = 0;
+                foreach (var wd in q1)
+                {
+                    if (wd.TempAvg <= 0)
+                    {
+                        turnCounter++;
+                        //lika med 5 så returnerar vi true och skriver ut en sträng med aktuellt datum.
+                        if (turnCounter == 5)
+                        {
+                            Console.WriteLine(wd.day + " " + wd.TempAvg);
+                            return true;
+                        }
+                    }
+                    else
+                    {
+                        //Räkningen startar om.
+                        turnCounter = 0;
+                    }
+                }
+                //går vi ur loopen och turnCounter inte gått upp till 5 fanns det inga datum som uppfyllde vilkåren.
+                //returneras false hittades inget datum.
+                if (turnCounter == 0)
+                {
+                    Console.WriteLine("Inget datum hittades");
+
+                }
+                return false;
+            }
+        }
         private static List<string> MetrologicalSeasonFall()
         {
             List<string> dates = new List<string>();
             using (var context = new EfContext())
             {
-                //
+                
                 var q1 = context.WeatherDataInfo
                     .Where(x => x.Location == "Ute" && x.DateAndTime > DateTime.Parse("2016-08-01"))
                     .GroupBy(x => x.DateAndTime.Date)
@@ -232,7 +348,7 @@ namespace InsertDataWeatherApp
                     if (wd.tempAVG < 10)
                     {
                         turnCounter++;
-                        dates.Add($"{wd.day:yyyy/MM/dd} \t {Math.Round(wd.tempAVG, 1)}");
+                        dates.Add($"{wd.day:yyyy-MM-dd} \t {Math.Round(wd.tempAVG, 1)}");
                         if (turnCounter == 5)
                         {
                             return dates;
@@ -253,7 +369,7 @@ namespace InsertDataWeatherApp
             using (var context = new EfContext())
             {
                 var q1 = context.WeatherDataInfo
-                    .Where(x => x.Location == $"{location}")
+                    .Where(x => x.Location == location)
                     .GroupBy(x => x.DateAndTime.Date)
                     .Select(x => new { DateAndTime = x.Key, HumAvg = x.Average(x => x.Humidity), TempAvg = x.Average(x => x.Temp) })//Plocka ut medeltemp och medelfuktighet.
                     .OrderBy(x => ((x.HumAvg - 78)) * (double)(x.TempAvg / 15) / 0.22);//Sortera i ordningen minst till störst efter formels resultat.
@@ -274,7 +390,7 @@ namespace InsertDataWeatherApp
                     //lägg till rätt typ av sträng i listan. 
                     if (Math.Round((mold.HumAvg - 78) * (double)(mold.TempAvg / 15) / 0.22) <= 0)
                     {
-                        moldList.Add($"{mold.DateAndTime: yyyy/MM/dd} \t {Math.Round((mold.HumAvg - 78) * (double)(mold.TempAvg / 15) / 0.22)}% Noll risk för mögel");
+                        moldList.Add($"{mold.DateAndTime: yyyy-MM-dd} \t {Math.Round((mold.HumAvg - 78) * (double)(mold.TempAvg / 15) / 0.22)}% Noll risk för mögel");
                     }
                 }
                 moldList.Add("\n");
@@ -285,7 +401,7 @@ namespace InsertDataWeatherApp
                     //kollar även där det ska vara högst risk för mögel om det finns minusvärden även där.
                     if (Math.Round((mold.HumAvg - 78) * (double)(mold.TempAvg / 15) / 0.22) <= 0)
                     {
-                        moldList.Add($"{mold.DateAndTime: yyyy/MM/dd} \t {Math.Round((mold.HumAvg - 78) * (double)(mold.TempAvg / 15) / 0.22)}% Noll risk för mögel");
+                        moldList.Add($"{mold.DateAndTime: yyyy-MM-dd} \t {Math.Round((mold.HumAvg - 78) * (double)(mold.TempAvg / 15) / 0.22)}% Noll risk för mögel");
                     }
                     //Om risken för mögel är över 0%
                     else if (Math.Round((mold.HumAvg - 78) * (double)(mold.TempAvg / 15) / 0.22) > 0)
@@ -296,124 +412,8 @@ namespace InsertDataWeatherApp
                 return moldList;
             }
         }
-        private static bool MetrologicalSeasonWinter()
-        {
-            using (var context = new EfContext())
-            {
-                var q1 = context.WeatherDataInfo
-                    .Where(x => x.Location == "Ute" && x.DateAndTime > DateTime.Parse("2016-08-01"))
-                    .GroupBy(x => x.DateAndTime.Date)
-                    .Select(x => new { day = x.Key, TempAvg = x.Average(x => x.Temp) })
-                    .OrderBy(x => x.day);
-
-                int turnCounter = 0;
-                foreach (var wd in q1)
-                {
-                    if (wd.TempAvg <= 0)
-                    {
-                        turnCounter++;
-                        //lika med true så returnerar vi true och skriver ut en sträng med aktuellt datum.
-                        if (turnCounter == 5)
-                        {
-                            Console.WriteLine(wd.day + " " + wd.TempAvg);
-                            return true;
-                        }   
-                    }
-                    else
-                    {
-                        //Räkningen startar om.
-                        turnCounter = 0;
-                    }    
-                }
-                //går vi ur loopen och turnCounter inte gått upp till 5 fanns det inga datum som uppfyllde vilkåren.
-                //returneras false hittades inget datum.
-                if (turnCounter==0)
-                {
-                    Console.WriteLine("No data found");
-                    
-                }
-                return false;
-            }
-        }
-        public static List<string> HumidMethod(string location)
-        {
-            using (var context = new EfContext())
-            {
-                List<string> avgHumidity = new List<string>();
-
-                var resultSet = context.WeatherDataInfo
-                    .Where(x => x.Location == $"{location}")
-                    .GroupBy(x => x.DateAndTime.Date)
-                    .Select(x => new { DateAndTime = x.Key, AVGHumid = x.Average(x => x.Humidity) });
-                    
-
-                var topResult = resultSet
-                    .OrderByDescending(x => x.AVGHumid)
-                    .Take(10);
-                var bottomResult = resultSet
-                    .OrderBy(x => x.AVGHumid)//För att få de nedersta raderna.
-                    .Take(10)
-                    .OrderByDescending(x => x.AVGHumid);//För att få de nedersta raderna i fallande ordning.
-
-                InsideorOutSide(location);
-                avgHumidity.Add("Tio dagarna med högst fuktighet");
-                avgHumidity.Add("*******************************");
-                foreach (var result in topResult)
-                {
-                    avgHumidity.Add($"{result.DateAndTime:yyyy/MM/dd} \t {Math.Round(result.AVGHumid, 1)} %");
-                }
-                avgHumidity.Add("\n");
-                avgHumidity.Add("Tio dagarna med lägst fuktighet");
-                avgHumidity.Add("*******************************");
-
-                foreach (var result in bottomResult)
-                {
-                    avgHumidity.Add($"{result.DateAndTime:yyyy/MM/dd} \t {Math.Round(result.AVGHumid,1)} %");
-                }
-                return avgHumidity;
-            }
-
-        }
-        public static List<string> FromWarmToCold(string location)
-        {
-            using (var context = new EfContext())
-            {
-                List<string> avgTemperatures = new List<string>();
-
-                //väljer ut de poster beroende av vilken sträng input vi får (inne eller ute)
-                //Sen grupperar vi det på datum-fältet. 
-                var resultSet = context.WeatherDataInfo
-                    .Where(x => x.Location == $"{location}")
-                    .GroupBy(x => x.DateAndTime.Date)
-                    .Select(x => new { DateAndTime = x.Key, AvgTemp = x.Average(x => x.Temp) }) //Hämtar ut medeltemp för de grupperande datumen.
-                    .OrderByDescending(x => x.AvgTemp);
-
-                var topResult = resultSet
-                    .Take(10);
-                var bottomResult = resultSet
-                    .OrderBy(x => x.AvgTemp)
-                    .Take(10)
-                    .OrderByDescending(x => x.AvgTemp);
-
-                InsideorOutSide(location);
-                avgTemperatures.Add("Tio dagarna med högst temp");
-                avgTemperatures.Add("**************************");
-                foreach (var result in topResult)
-                {
-                    avgTemperatures.Add($"{result.DateAndTime:yyyy/MM/dd} \t {Math.Round(result.AvgTemp, 1)} °C");//skriver in värdena till strängar i listan.
-                }
-                avgTemperatures.Add("\n");
-                avgTemperatures.Add("Tio dagarna med lägst temp");
-                avgTemperatures.Add("**************************");
-                foreach (var result in bottomResult)
-                {
-                    avgTemperatures.Add($"{result.DateAndTime:yyyy/MM/dd} \t {Math.Round(result.AvgTemp, 1)} °C");
-                }
-                return avgTemperatures;
-            }
-        }
-
-
+        
+ 
         public static void ReadCSVFile(string filePath)
         {
             //Samlar alla rader i filen till en sträng-array.
